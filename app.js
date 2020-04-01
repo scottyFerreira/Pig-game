255b313d3da5e1967b78efe3e36c79b9c35cdbcd
Scotty_F <scottymferreira@gmail.com>
2020-04-01 13:14:58 -0400

    challenge 2 complete score selection available

--- app.js
+++ app.js
@@ -61,6 +61,17 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     //update the UI 
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
 
+    var input = document.querySelector('.winScore').value;
+    var winScore;
+    
+    //Undefined, 0, null or "" are coerced to false
+    //anything else is coerced true
+    if(input) {
+        winScore = input;
+    } else {
+        winScore = 100;
+    }
+
     //check if player won the game
     if(scores[activePlayer] >= 20) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
